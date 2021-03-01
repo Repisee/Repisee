ac20901dd39ce8ffb60db13f10a9b585bbf7913b
Iskak Mantyubetov <ayzik55@gmail.com>
2021-03-01 10:41:21 -0500

    added new model post

--- posts/models.py
+++ posts/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 
 class Post(models.Model):
-    # title = models.CharField(max_length=120)
+    title = models.CharField(max_length=120)
     desription = models.TextField(blank=True, null=True)
     number = models.DecimalField(max_digits=1000, decimal_places=2)
     comment = models.TextField()
