cce5a99daeac3350652bbf9b3c3384e2470b4a01
Iskak Mantyubetov <ayzik55@gmail.com>
2021-03-02 09:26:01 -0500

    added new model post

--- posts/models.py
+++ posts/models.py
@@ -7,4 +7,4 @@ class Post(models.Model):
     title = models.CharField(max_length=120)
     desription = models.TextField(blank=True, null=True)
     number = models.DecimalField(max_digits=1000, decimal_places=2)
-    comment = models.TextField()
+    # comment = models.TextField()
